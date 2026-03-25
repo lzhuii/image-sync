1d4a0264b9631743133e58b51fee164fa7e2c1eb
hui <cn_hui@163.com>
2026-03-25 20:15:45 +0800

    更新 sync.sh 脚本，确保在同步镜像时过滤掉注释行

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-while IFS='|' read -r namespace src; do
+grep -v "#" images.txt | while IFS='|' read -r namespace src; do
     image=${src##*/}
     dst="$REGISTRY/$namespace/$image"
     echo "$src -> $dst"
     skopeo copy -a -q docker://$src docker://$dst
-done < <(grep -v "#" images.txt)
+done
