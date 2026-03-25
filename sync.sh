d899641f2c57e5b4e744d735ef38bf7079ef15cd
hui <cn_hui@163.com>
2026-03-25 20:12:36 +0800

    重构镜像同步逻辑，将同步脚本移至单独的 sync.sh 文件

---
+++ sync.sh
@@ -0,0 +1,6 @@
+while IFS='|' read -r namespace src; do
+    image=${src##*/}
+    dst="$REGISTRY/$namespace/$image"
+    echo "$src -> $dst"
+    skopeo copy -a -q docker://$src docker://$dst
+done < <(grep -v "#" images.txt)
