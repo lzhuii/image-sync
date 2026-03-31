25da0b50d1b14984a42c4a2f903c9e57bfc2eb4c
hui <cn_hui@163.com>
2026-03-31 11:16:09 +0800

    更新 sync.yml，升级 actions/checkout 版本至 v5；更新 sync.sh，优化图像同步逻辑以跳过空命名空间

--- sync.sh
+++ sync.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-grep -v "#" images.txt | while IFS='|' read -r namespace src; do
-    [ -z "$namespace" ] && continue
+grep -v -E "^(#|$)" images.txt | while IFS='|' read -r namespace src; do
     image=${src##*/}
     dst="$REGISTRY/$namespace/$image"
+    src_digest=$(skopeo inspect --raw docker://$src | sha256sum | cut -d ' ' -f1)
+    dst_digest=$(skopeo inspect --raw docker://$dst | sha256sum | cut -d ' ' -f1)
+    if [ "$src_digest" = "$dst_digest" ]; then
+        echo "$dst exists"
+        continue
+    fi
     echo "$src -> $dst"
     skopeo copy -a -q docker://$src docker://$dst
 done
